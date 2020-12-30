4594e59c01bd4a780d2cee12ba13d1e17bc39194
umitkadiroglu <38588465+umitkadiroglu@users.noreply.github.com>
2020-12-30 17:07:04 +0300

    grup oluştur taslak

--- app/src/main/java/com/emugamestudios/groupup/AddActivity.java
+++ app/src/main/java/com/emugamestudios/groupup/AddActivity.java
@@ -4,10 +4,18 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class AddActivity extends AppCompatActivity {
+    ImageView image_group;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,38 @@ public class AddActivity extends AppCompatActivity {
         assert getSupportActionBar() != null;   //null check
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);   //show back button
         getSupportActionBar().setTitle(R.string.create_group);
+
+        image_group = findViewById(R.id.image_group);
+
+        Glide.with(AddActivity.this).load(R.drawable.ic_add_image_grey).circleCrop().into(image_group);
+
+        image_group.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(AddActivity.this, R.style.BottomSheetDialogTheme);
+                View bottomSheetView = LayoutInflater.from(getApplicationContext())
+                        .inflate(R.layout.layout_bottom_sheet, (LinearLayout)findViewById(R.id.bottomSheetContainer));
+
+                bottomSheetView.findViewById(R.id.button_gallery).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //galeri açılacak
+                        bottomSheetDialog.dismiss();
+                    }
+                });
+
+                bottomSheetView.findViewById(R.id.button_camera).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //camera açılacak
+                        bottomSheetDialog.dismiss();
+                    }
+                });
+                bottomSheetDialog.setContentView(bottomSheetView);
+                bottomSheetDialog.show();
+            }
+        });
+
     }
 
     @Override
