e896ee7d726d3ac409dba30fe0c6aae973a0599c
umitkadiroglu <38588465+umitkadiroglu@users.noreply.github.com>
2021-01-10 23:39:36 +0300

    kullanıcı bul eklendi

--- app/src/main/java/com/emugamestudios/groupup/SearchFragment.java
+++ app/src/main/java/com/emugamestudios/groupup/SearchFragment.java
@@ -3,7 +3,10 @@ package com.emugamestudios.groupup;
 import android.app.ActionBar;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,7 +14,22 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchFragment extends Fragment {
+    RecyclerView users_recyclerview;
+    AdapterUsers adapterUsers;
+    List<ModelUser> userList;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -20,7 +38,42 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view =  inflater.inflate(R.layout.fragment_search, container, false);
+        users_recyclerview = view.findViewById(R.id.users_recyclerview);
+        users_recyclerview.setHasFixedSize(true);
+        users_recyclerview.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        userList = new ArrayList<>();
+
+        getAllUsers();
+        return view;
+    }
+
+    private void getAllUsers() {
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    ModelUser modelUser = dataSnapshot.getValue(ModelUser.class);
+
+                    if (!modelUser.getUid().equals(firebaseUser.getUid())){
+                        userList.add(modelUser);
+                    }
+
+                    adapterUsers = new AdapterUsers(getActivity(), userList);
+                    users_recyclerview.setAdapter(adapterUsers);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 
     @Override
